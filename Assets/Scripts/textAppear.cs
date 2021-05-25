b222c80d53e989304e533b392f43141b12e898e0
Gillian <gillianyue1999@hotmail.com>
2021-05-26 00:53:28 +0800

    sfx and texts final

--- Assets/Scripts/textAppear.cs
+++ Assets/Scripts/textAppear.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,12 +8,15 @@ public class textAppear : MonoBehaviour
     private bool status; //when true, is showing
     private Animator myAnim;
     private SpriteRenderer textSprite;
+    public float inDistt;
 
     void Start()
     {
         myAnim = GetComponent<Animator>();
         textSprite = GetComponent<SpriteRenderer>();
         textSprite.color = new Color(0, 0, 0, 0); //hide
+
+if(inDistt == 0) inDistt = 10;
     }
 
     void Update()
@@ -31,11 +34,11 @@ public class textAppear : MonoBehaviour
 
     public bool inDist()
     {
-        return (Vector3.Distance(transform.position, cam.transform.position) < 10);
+        return (Vector3.Distance(transform.position, cam.transform.position) < inDistt);
     }
 
     public bool outDist()
     {
-        return (Vector3.Distance(transform.position, cam.transform.position) > 14);
+        return (Vector3.Distance(transform.position, cam.transform.position) > inDistt+4);
     }
 }
